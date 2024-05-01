cc4d1abf37977d7a1f7554f8d15fb9bd2c536d4d
Graham <graham.a.ross@gmail.com>
2024-05-01 13:02:13 -0400

    Update Defines.h

--- code/ESP32/TS45ToMQTT/include/Defines.h
+++ code/ESP32/TS45ToMQTT/include/Defines.h
@@ -2,7 +2,7 @@
 #pragma once
 
 #define TAG "TS45ToMQTT"
-#define CONFIG_VERSION "V1.0.1" // major.minor.build (major or minor will invalidate the configuration)
+#define CONFIG_VERSION "V1.1.0" // major.minor.build (major or minor will invalidate the configuration)
 #define HOME_ASSISTANT_PREFIX "homeassistant" // MQTT prefix used in autodiscovery
 #define STR_LEN 255                            // general string buffer size
 #define CONFIG_LEN 32                         // configuration string buffer size
